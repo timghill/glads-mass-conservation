c5e683935c84611a2bf98fa5e98f293af0f7ebb0
tghill <tghill@uwaterloo.ca>
2023-09-21 15:21:09 -0700

    Scaling tests

--- src/matlab_noscaling/plot_outs.m
+++ src/matlab_noscaling/plot_outs.m
@@ -1,17 +1,18 @@
-outs = load('RUN/seasonal_001.mat');
+outs = load('RUN/seasonal_001_norho.mat');
 
 dmesh = load('../data/mesh.mat');
 
 tindex = 170;
-figure('Units', 'inches', 'Position', [3, 2, 8, 3])
+f = figure('Units', 'inches', 'Position', [3, 2, 8, 3]);
 patch('Faces', dmesh.tri.connect, 'Vertices', dmesh.tri.nodes, ...
     'FaceVertexCData', outs.phis(:, tindex), 'EdgeColor', 'none', ...
     'FaceColor', 'flat');
 axis image
 cb = colorbar;
 cb.Label.String = '\phi (MPa)';
-print('phi_statevariables', '-dpng', '-r600')
-% 
-% figure
-% plot(mean(outs.phis))
-% cmocean('dense')
+print(f, 'phi_norho', '-dpng', '-r600')
+
+f2 = figure;
+plot(mean(outs.phis))
+cmocean('dense')
+print(f2, 'phi_norho_time', '-dpng', '-r600')
